f29097c3366731c70c0aa903d4863c8967eedf90
Akshat Gupta <a.akshat903@gmail.com>
2022-10-09 11:36:47 +0530

    cyclic sort algorithm added

---
+++ CyclicSort.java
@@ -0,0 +1,55 @@
+// CYCLIC SORT
+
+package com.Rhythm;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 4, 3, 2, 1};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+//    static void cyclic(int[] arr){
+//        int i = 0;
+//        while (i < arr.length){
+//            int correct = arr[i] - 1;
+//            if (arr[i] != arr[correct]){
+//                swap(arr, i, correct);
+//            }
+//            else{
+//                i++;
+//            }
+//        }
+//    }
+//
+//    static void swap(int[] arr, int first, int second){
+//        int temp = arr[first];
+//        arr[first] = arr[second];
+//        arr[second] = temp;
+//    }
+
+    static void cyclic(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]){
+                swap(arr, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
